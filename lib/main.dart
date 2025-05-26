7a4a0b4bdaf475e8674b71bd52dd7bca9a1ab709
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-05-25 21:13:56 -0400

    post skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -68,8 +68,58 @@ class Feed extends StatelessWidget {
       itemCount: 5,
       separatorBuilder: (BuildContext context, int index) => const Divider(),
       itemBuilder: (BuildContext context, int index) {
-        return Placeholder();
+        return Post();
       },
+    );
+  }
+}
+
+class Post extends StatelessWidget {
+  const Post({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: NetworkImage("https://placehold.co/48/png"),
+                          ),
+                        ),
+                        width: 32,
+                        height: 32,
+                      ),
+                      SizedBox(width: 8),
+                      Text("username"),
+                    ],
+                  ),
+                  Text("1 day ago"),
+                ],
+              ),
+              SizedBox(height: 8),
+              Chip(label: Text("intention"), padding: EdgeInsets.all(0)),
+            ],
+          ),
+        ),
+        Image(
+          image: NetworkImage("https://placehold.co/400/png"),
+          fit: BoxFit.fill,
+        ),
+        Padding(padding: EdgeInsets.all(8), child: Text("description")),
+      ],
     );
   }
 }
