a985df46b2ea182c161343fa0b36f2629686c40e
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-06-08 01:14:57 -0400

    respond to follows

--- lib/pages/notifications.dart
+++ lib/pages/notifications.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intentions_flutter/models/follow.dart';
+import 'package:intentions_flutter/providers/auth_user.dart';
+import 'package:intentions_flutter/providers/follows.dart';
 import 'package:intentions_flutter/widgets/profile_pic.dart';
 
 class Notifications extends ConsumerWidget {
@@ -7,20 +10,42 @@ class Notifications extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final userId = ref.watch(authUserProvider).user?.uid;
+    final List<Follow> follows =
+        ref.watch(followsToUserProvider(userId)).value ?? [];
+
+    final respondToFollow = ref.read(respondToFollowProvider);
+
     return Scaffold(
       body: ListView(
         children: [
-          for (var _ in Iterable.generate(5))
+          for (final follow in follows)
             ListTile(
-              title: Text("user requested to follow you"),
-              leading: ProfilePic(image: "TODO"),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(icon: Icon(Icons.check), onPressed: () {}),
-                  IconButton(icon: Icon(Icons.close), onPressed: () {}),
-                ],
+              title: Text(
+                follow.status == FollowStatus.pending
+                    ? "${follow.fromUser.username} requested to follow you"
+                    : '${follow.fromUser.username} followed you',
               ),
+              leading: ProfilePic(image: follow.fromUser.image),
+              trailing: follow.status == FollowStatus.pending
+                  ? Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.check),
+                          onPressed: () {
+                            respondToFollow(RespondAction.accept);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () {
+                            respondToFollow(RespondAction.decline);
+                          },
+                        ),
+                      ],
+                    )
+                  : null,
             ),
         ],
       ),
--- lib/providers/follows.dart
+++ lib/providers/follows.dart
@@ -1,12 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 import 'package:intentions_flutter/firebase.dart';
 import 'package:intentions_flutter/models/follow.dart';
+import 'package:intentions_flutter/providers/auth_user.dart';
 
-final followsProvider = FutureProvider.family<List<Follow>, String>((
+final followsToUserProvider = FutureProvider.family<List<Follow>, String?>((
   ref,
   toUser,
 ) async {
+  if (toUser == null) {
+    return [];
+  }
+
   final follows = await firebase.db.collection('follows/$toUser/from').get();
 
   return follows.docs.map((follow) => Follow.fromJson(follow.data())).toList();
+});
+
+enum RespondAction { accept, decline }
+
+Future<void> respondToFollow(Ref ref, RespondAction action) async {
+  final user = ref.read(authUserProvider).user;
+  final token = await user?.getIdToken();
+
+  await http.post(
+    Uri.http('localhost:3001', '/posts'),
+    headers: {'Authorization': token ?? '', 'Content-Type': 'application/json'},
+    body: jsonEncode({
+      'action': action == RespondAction.accept ? 'accept' : 'decline',
+    }),
+  );
+}
+
+final respondToFollowProvider = Provider((ref) {
+  return (RespondAction action) async {
+    await respondToFollow(ref, action);
+  };
 });
