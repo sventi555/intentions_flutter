e6975f85570875933a99bc2a14c6d28b944f2e20
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-06-17 12:25:01 -0400

    sign in form validation

--- lib/pages/auth/sign_in.dart
+++ lib/pages/auth/sign_in.dart
@@ -16,7 +16,12 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  var passwordVisible = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  String? emailErr;
+  String? passwordErr;
+  String? formErr;
 
   @override
   void dispose() {
@@ -27,12 +32,22 @@ class _SignInState extends State<SignIn> {
 
   void onSubmit() async {
     try {
-      firebase.auth.signInWithEmailAndPassword(
+      await firebase.auth.signInWithEmailAndPassword(
         email: emailController.text,
         password: passwordController.text,
       );
     } on FirebaseAuthException catch (e) {
-      print(e.code);
+      setState(() {
+        if (e.code == 'invalid-email') {
+          emailErr = 'Enter a valid email address';
+        } else if (e.code == 'user-not-found') {
+          emailErr = 'No user with that email';
+        } else if (e.code == 'wrong-password') {
+          passwordErr = 'Incorrect password';
+        } else {
+          formErr = 'Something went wrong, please try again';
+        }
+      });
     }
   }
 
@@ -41,43 +56,76 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(8),
-        child: Column(
-          spacing: 16,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Column(
-              children: [
-                Text("Intentions", style: TextStyle(fontSize: 32)),
-                Text("act intentionally", style: TextStyle(fontSize: 16)),
-              ],
-            ),
-            Column(
-              spacing: 4,
-              children: [
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    label: Text("email"),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            spacing: 16,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                children: [
+                  Text("Intentions", style: TextStyle(fontSize: 32)),
+                  Text("act intentionally", style: TextStyle(fontSize: 16)),
+                ],
+              ),
+              Column(
+                spacing: 4,
+                children: [
+                  TextFormField(
+                    forceErrorText: emailErr,
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      label: Text("email"),
+                    ),
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return "Enter an email";
+                      }
+                      return null;
+                    },
                   ),
+                  PasswordInput(
+                    controller: passwordController,
+                    forceErrorText: passwordErr,
+                  ),
+                ],
+              ),
+              if (formErr != null)
+                Text(
+                  formErr!,
+                  style: TextStyle(color: Theme.of(context).colorScheme.error),
                 ),
-                PasswordInput(controller: passwordController),
-              ],
-            ),
-            FilledButton(onPressed: onSubmit, child: Text("Sign in")),
-            Column(
-              children: [
-                Text("New user?"),
-                TextButton(
-                  child: Text("Sign up"),
-                  onPressed: () {
-                    context.go('/signup');
-                  },
-                ),
-              ],
-            ),
-          ],
+              FilledButton(
+                onPressed: () {
+                  setState(() {
+                    emailErr = null;
+                    passwordErr = null;
+                    formErr = null;
+                  });
+
+                  // need to wait for forcedErrorText to clear
+                  // otherwise validate will report "false" for some reason...
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    onSubmit();
+                  });
+                },
+                child: Text("Sign in"),
+              ),
+              Column(
+                children: [
+                  Text("New user?"),
+                  TextButton(
+                    child: Text("Sign up"),
+                    onPressed: () {
+                      context.go('/signup');
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
